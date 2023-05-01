35f0449b84b7c400ec10307cd48419fcf6ca8758
Victor Luz <victorluzribeiro@gmail.com>
2023-05-01 00:38:30 -0300

    Project created

---
+++ src/test/java/com/victorluz/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorluz.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
